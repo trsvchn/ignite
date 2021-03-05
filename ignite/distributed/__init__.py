6ba1242350197129d9a9d7e3480a8bc98bcc5c01
Taras Savchyn <30748114+trsvchn@users.noreply.github.com>
2021-03-05 17:43:42 +0200

    Update __init__.py

--- ignite/distributed/__init__.py
+++ ignite/distributed/__init__.py
@@ -1,4 +1,3 @@
 from ignite.distributed.auto import *
 from ignite.distributed.comp_models import native, xla
 from ignite.distributed.launcher import Parallel
-from ignite.distributed.utils import *
